92c6d42ae836405655abe155ac1c9cecaac7bdb8
Valcinei Jr <hakjog@gmail.com>
2022-05-14 20:42:13 -0300

    fix: corringindo o erro sem nexo

--- src/contexts/ThemeProvider.tsx
+++ src/contexts/ThemeProvider.tsx
@@ -1,9 +1,17 @@
 import { useTheme } from '@/hooks/useTheme';
+import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-function MyThemeProvider({ children }) {
+function MyThemeProvider({ children }): JSX.Element {
   const { theme } = useTheme();
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+
+  const Provider: any = ThemeProvider;
+
+  return (
+    <Provider theme={theme}>
+      <React.Fragment>{children} </React.Fragment>
+    </Provider>
+  );
 }
 
 export default MyThemeProvider;
--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
+const GlobalStyle: any = createGlobalStyle`
 
 @keyframes fade-bottom {
   from {
